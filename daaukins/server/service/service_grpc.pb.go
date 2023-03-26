a7948b0ef2355ea29ea287cc67879d00d132786a
Andreas Wachs <andreas@wachs.dk>
2023-03-26 10:06:45 +0200

    Compiled protobuf

--- daaukins/server/service/service_grpc.pb.go
+++ daaukins/server/service/service_grpc.pb.go
@@ -27,6 +27,7 @@ type ServiceClient interface {
 	GetLab(ctx context.Context, in *GetLabRequest, opts ...grpc.CallOption) (*GetLabResponse, error)
 	GetLabs(ctx context.Context, in *GetLabsRequest, opts ...grpc.CallOption) (*GetLabsResponse, error)
 	RemoveLab(ctx context.Context, in *RemoveLabRequest, opts ...grpc.CallOption) (*RemoveLabResponse, error)
+	GetServerMode(ctx context.Context, in *GetServerModeRequest, opts ...grpc.CallOption) (*GetServerModeResponse, error)
 }
 
 type serviceClient struct {
@@ -82,6 +83,15 @@ func (c *serviceClient) RemoveLab(ctx context.Context, in *RemoveLabRequest, opt
 	return out, nil
 }
 
+func (c *serviceClient) GetServerMode(ctx context.Context, in *GetServerModeRequest, opts ...grpc.CallOption) (*GetServerModeResponse, error) {
+	out := new(GetServerModeResponse)
+	err := c.cc.Invoke(ctx, "/service.service/GetServerMode", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ServiceServer is the server API for Service service.
 // All implementations must embed UnimplementedServiceServer
 // for forward compatibility
@@ -91,6 +101,7 @@ type ServiceServer interface {
 	GetLab(context.Context, *GetLabRequest) (*GetLabResponse, error)
 	GetLabs(context.Context, *GetLabsRequest) (*GetLabsResponse, error)
 	RemoveLab(context.Context, *RemoveLabRequest) (*RemoveLabResponse, error)
+	GetServerMode(context.Context, *GetServerModeRequest) (*GetServerModeResponse, error)
 	mustEmbedUnimplementedServiceServer()
 }
 
@@ -112,6 +123,9 @@ func (UnimplementedServiceServer) GetLabs(context.Context, *GetLabsRequest) (*Ge
 }
 func (UnimplementedServiceServer) RemoveLab(context.Context, *RemoveLabRequest) (*RemoveLabResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method RemoveLab not implemented")
+}
+func (UnimplementedServiceServer) GetServerMode(context.Context, *GetServerModeRequest) (*GetServerModeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetServerMode not implemented")
 }
 func (UnimplementedServiceServer) mustEmbedUnimplementedServiceServer() {}
 
@@ -216,6 +230,24 @@ func _Service_RemoveLab_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Service_GetServerMode_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetServerModeRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ServiceServer).GetServerMode(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/service.service/GetServerMode",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ServiceServer).GetServerMode(ctx, req.(*GetServerModeRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Service_ServiceDesc is the grpc.ServiceDesc for Service service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -242,6 +274,10 @@ var Service_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "RemoveLab",
 			Handler:    _Service_RemoveLab_Handler,
+		},
+		{
+			MethodName: "GetServerMode",
+			Handler:    _Service_GetServerMode_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
