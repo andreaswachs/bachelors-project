bba8ba3a4efe2c45ab7b6094ab2462c7563e69f0
Andreas Wachs <andreas@wachs.dk>
2023-04-11 22:27:21 +0200

    Redo regex and refactor

--- daaukins/server/utils/utils.go
+++ daaukins/server/utils/utils.go
@@ -12,7 +12,7 @@ import (
 var (
 	ErrorMemAvailableNotFoundInMeminfo = fmt.Errorf("could not find MemAvailable in /proc/meminfo")
 	meminfoFile                        = "/proc/meminfo"
-	MemAvailableRegex                  = regexp.MustCompile(`(?m)^MemAvailable:\s+(\d+) kB$`)
+	memAvailableRegex                  = regexp.MustCompile(`(?m)^MemAvailable\:\s*(\d+)\s+kB\s*$`)
 )
 
 func RandomName() string {
@@ -36,19 +36,15 @@ func GetAvailableMemory() (int, error) {
 	var memFree int
 
 	// Find the MemAvailable line
-	matches := MemAvailableRegex.FindSubmatch(contents)
+	matches := memAvailableRegex.FindSubmatch(contents)
 	// Matches are the full match and the first capture group
-	if len(matches) == 2 {
-		// Convert the MemAvailable value to an integer
-		memFree, err = strconv.Atoi(string(matches[1]))
-		if err != nil {
-			return 0, err
-		}
+	if len(matches) != 2 {
+		return 0, ErrorMemAvailableNotFoundInMeminfo
 	}
 
-	if memFree == 0 {
-		return 0, fmt.Errorf("could not find MemAvailable in /proc/meminfo")
-	}
+	// Convert the MemAvailable value to an integer
+	// We can ignore the error because the regex only matches numbers
+	memFree, _ = strconv.Atoi(string(matches[1]))
 
 	// Convert from kilobytes to megabytes
 	memFree /= 1024
@@ -64,11 +60,4 @@ func DeleteFiles(files []string) error {
 	}
 
 	return nil
-}
-
-// SetMeminfoFile sets the path to the meminfo file to be used by the
-// GetAvailableMemory function.
-// This should only be used for testing purposes.
-func SetMeminfoFile(file string) {
-	meminfoFile = file
 }
