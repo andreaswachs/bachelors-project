8d1d31bbf49477f2bcdaa5058625ee60ba278bcf
Andreas Wachs <andreas@wachs.dk>
2023-04-11 11:17:35 +0200

    Add server id to schedling

--- daaukins/client/api/schedule.go
+++ daaukins/client/api/schedule.go
@@ -2,9 +2,9 @@ package api
 
 import service "github.com/andreaswachs/daaukins-service"
 
-func ScheduleLab(lab string) (*service.ScheduleLabResponse, error) {
+func ScheduleLab(lab, serverId string) (*service.ScheduleLabResponse, error) {
 	ctx, cancelFunc := newCtx()
 	defer cancelFunc()
 
-	return getClient().ScheduleLab(ctx, &service.ScheduleLabRequest{Lab: lab})
+	return getClient().ScheduleLab(ctx, &service.ScheduleLabRequest{Lab: lab, ServerId: serverId})
 }
