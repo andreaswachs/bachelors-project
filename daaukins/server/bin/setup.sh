3696ec1bcc7fb2896df867314f073aedbaead1f8
Andreas Wachs <andreas@wachs.dk>
2023-05-12 20:53:03 +0200

    Remove setup file

--- daaukins/server/bin/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker pull networkboot/dhcpd:1.2.0
-docker pull coredns/coredns:1.10.0
-docker pull andreaswachs/daaukins-server:latest
-docker pull andreaswachs/placeholder_vuln_server:latest
