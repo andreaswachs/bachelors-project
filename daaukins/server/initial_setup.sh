0263993d8a045c902fdbc6cd3f5e3f5bf0da0d7e
Andreas Wachs <andreas@wachs.dk>
2023-05-12 17:59:31 +0200

    Upodate

--- daaukins/server/initial_setup.sh
+++ daaukins/server/initial_setup.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env sh
 
-echo "Ensure you are running this script with root privileges"
+echo "This setup script will require root access for some commands"
 
-apt install -y make docker docker-compose
-ufw enable
-ufw allow ssh
-ufw allow 50052/tcp
-ufw allow 40000:50000/tcp
+# Installing dependencies
+sudo apt update
+sudo apt install -y make docker docker-compose
+
+# Configure Docker
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo chmod 666 /var/run/docker.sock
+#
+# Creating the daaukins user
+sudo useradd -m daaukins
+usermod -a -G sudo daaukins
+usermod -a -G docker daaukins
+
+# ufw should be provided with newer Ubuntu server releases
+sudo ufw enable
+sudo ufw allow ssh
+sudo ufw allow 50052/tcp
+sudo ufw allow 40000:50000/tcp
+
+# Pull docker containers mentioned in yaml files
+make pull-images
